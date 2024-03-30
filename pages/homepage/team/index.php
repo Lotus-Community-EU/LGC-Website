9f702a6b0e98a59ca13b650e8fb0920b58eddfdb
SpoonyUK <nilsgaminglog3@gmail.com>
2024-03-30 21:42:22 +0100

    Update index.php

--- pages/homepage/team/index.php
+++ pages/homepage/team/index.php
@@ -8,32 +8,36 @@ foreach($all_ranks as $rank) {
     }
     ?>
     <div class="container-fluid">
-        <div class="row mt-5 justify-content-center">
-            <h3 class="text-center"><span style="color: <?= $rank['colour'];?>;"><?= $rank['name'];?></span></h3>
-            <?php
-            $get_users = Rank::getAllUsersByRank($rank['id']);
-            if($get_users == 0) {
-                //echo '<span class="text-center">Rank has no Members!</span>';
-                continue;
-            }
-            else {
-                foreach($get_users as $get_user) {
-                    if($get_user['team_hidden'] == 1) { continue; }
-                    ?>
-                    <div class="col-12 col-sm-6 col-lg-4 mt-3" style="float: none !important;">
-                        <div class="card text-white text-center mx-auto" style="width: 18rem; background: none; border: none;">
-                            <img src="/assets/images/avatar/<?= $get_user['avatar'];?>"  class="card-img-top" height="250" width="250">
-                            <div class="card-body">
-                                <h5 class="card-title"><a href="/profile/<?= $get_user['id'];?>" class="text-white text-decoration-none name_link" target="_blank"><?= $get_user['username'];?></a></h5>
-                                <p class="card-text text-decoration-underline"><?= $rank['name'];?></p>
+        <?php
+        $get_users = Rank::getAllUsersByRank($rank['id']);
+        if($get_users == 0) {
+            //echo '<span class="text-center">Rank has no Members!</span>';
+            continue;
+        }
+        else {
+            ?>
+            <div class="row mt-5 justify-content-center">
+                <h3 class="text-center"><span style="color: <?= $rank['colour'];?>;"><?= $rank['name'];?></span></h3>
+                <?php
+                    foreach($get_users as $get_user) {
+                        if($get_user['team_hidden'] == 1) { continue; }
+                        ?>
+                        <div class="col-12 col-sm-6 col-lg-4 mt-3" style="float: none !important;">
+                            <div class="card text-white text-center mx-auto" style="width: 18rem; background: none; border: none;">
+                                <img src="/assets/images/avatar/<?= $get_user['avatar'];?>"  class="card-img-top" height="250" width="250">
+                                <div class="card-body">
+                                    <h5 class="card-title"><a href="/profile/<?= $get_user['id'];?>" class="text-white text-decoration-none name_link" target="_blank"><?= $get_user['username'];?></a></h5>
+                                    <p class="card-text text-decoration-underline"><?= $rank['name'];?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <?php
-                }
+                        <?php
+                    }
+                    ?>
+                </div>
+            <?php
             }
             ?>
-        </div>
     </div>
 <?php } ?>
 
